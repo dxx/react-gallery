908b34a75c08ec038d69c6de4820d6e59fa046e6
code-mcx <1827939333@qq.com>
2017-10-14 18:46:57 +0800

    Delete

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Gallery from './components/Gallery';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Gallery />, document.getElementById('app'));
-registerServiceWorker();
